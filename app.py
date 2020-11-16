9103c8773b9fd6026c99db193c1f6d2db17a86af
MinhChauVanNguyen <minh.chau@outlook.co.nz>
2020-11-17 12:31:41 +1300

    add labels to graphs

--- app.py
+++ app.py
@@ -24,21 +24,36 @@ app = dash.Dash(
 )
 
 server = app.server
-app.config.suppress_callback_exceptions = True
+app.config.suppress_callback_exceptions = False
 
 # Read data
 df = pd.read_csv(
     'https://raw.githubusercontent.com/ine-rmotr-curriculum/FreeCodeCamp-Pandas-Real-Life-Example/master/data/sales_data.csv')
 
+# Remove UK data since it only has one state
 uk = 'United Kingdom'
 mask = df['Country'].str.contains(uk, na=False, regex=True, case=False)
 df = df[~mask]
 
+# fix the France state name so that it corresponds to the geojson file
+df = df.replace({'State': {
+    'Seine Saint Denis': 'Seine-Saint-Denis',#
+    'Loir et Cher': 'Loir-et-Cher',
+    'Seine (Paris)': 'Paris', #
+    'Hauts de Seine': 'Hauts-de-Seine',
+    "Val d'Oise": "Val-d'Oise",
+    'Seine et Marne': 'Seine-et-Marne',
+    'Val de Marne': 'Val-de-Marne',
+    'Pas de Calais': 'Pas-de-Calais',
+    'Garonne (Haute)': 'Haute-Garonne',
+    'Yveline': 'Yvelines'}}
+)
+# subset data based on the following columns
 df.drop(
     df.columns.difference(['Customer_Gender', 'Age_Group', 'Product_Category', 'Revenue', 'State', 'Country', 'Year']),
     1, inplace=True)
 
-
+# sort data from the least to most recent year
 df = df.sort_values(by=["Country", "Year"])
 
 year_list = df["Year"].unique().tolist()
@@ -52,6 +67,13 @@ for i in country_df['Country'].unique().tolist():
     country = country_df[country_df['Country'] == i]
     my_dict[i] = country['State'].unique().tolist()
 
+# rename the levels of Gender column
+df['Customer_Gender'] = df['Customer_Gender'].replace({'F': 'Female', 'M': 'Male'})
+
+all_options = {
+    'Age_Group': df['Age_Group'].unique().tolist(),
+    'Customer_Gender': df['Customer_Gender'].unique().tolist()
+}
 
 def description_card():
     return html.Div(
@@ -60,30 +82,19 @@ def description_card():
             html.H3("Title"),
             html.Div(
                 id="intro",
-                children="Explore clinic patient volume by time of day, waiting time, and care score. Click on the heatmap to visualize patient experience at different time points.",
+                children="Explore clinic patient volume by time of day, waiting time, and care score."
             ),
             html.Br(),
             html.P("Select Country"),
             dcc.Dropdown(id="slct_country",
                          options=[{'label': c, 'value': c} for c in my_dict.keys()],
                          multi=False,
-                         value="Canada",
+                         value="France",
                          clearable=False
                          ),
             html.Br(),
             html.Div(id='state_label', style={'font-weight': 'bold', 'margin-bottom': '5px'}),
             dcc.Dropdown(id="slct_state", clearable=False),
-            html.Br(),
-            html.P("Grouped by:"),
-            dcc.Dropdown(
-                id='slct_group',
-                options=[
-                    {'label': 'Age group', 'value': 'Age_Group'},
-                    {'label': 'Gender', 'value': 'Customer_Gender'},
-                ],
-                value='Age_Group',
-                clearable=False
-            ),
             html.Br(),
             html.P("Select year"),
             dcc.Dropdown(id="slct_year",
@@ -92,6 +103,17 @@ def description_card():
                          multi=True,
                          clearable=False
                          ),
+            html.Br(),
+            html.P("Select Group"),
+            dcc.Dropdown(
+                id='slct_group',
+                options=[{'label': k, 'value': k} for k in all_options.keys()],
+                value='Age_Group',
+                clearable=False
+            ),
+            html.Br(),
+            html.P("Select sub group"),
+            dcc.RadioItems(id='slct_subgrp'),
         ],
     )
 
@@ -167,6 +189,11 @@ app.layout = html.Div(
 def set_states_options(selected_country):
     return [{'label': i, 'value': i} for i in my_dict[selected_country]]
 
+@app.callback(
+    Output(component_id='slct_subgrp', component_property='options'),
+    [Input(component_id='slct_group', component_property='value')])
+def set_group_options(selected_group):
+    return [{'label': i, 'value': i} for i in all_options[selected_group]]
 
 @app.callback(
     Output(component_id='slct_state', component_property='value'),
@@ -174,6 +201,11 @@ def set_states_options(selected_country):
 def set_states_value(country_options):
     return country_options[0]['value']
 
+@app.callback(
+    Output(component_id='slct_subgrp', component_property='value'),
+    [Input(component_id='slct_subgrp', component_property='options')])
+def set_subgroup_value(group_option):
+    return group_option[0]['value']
 
 # callbacks for outputs
 @app.callback(
@@ -204,9 +236,17 @@ def update_output(selected_country, selected_state, selected_group, selected_yea
     grouped_df = df.loc[
         (df["Country"] == selected_country) & (df["State"] == selected_state) & (df["Year"].isin(selected_year))]
 
-    grouped_df = grouped_df.groupby(["Country", "State", selected_group, "Product_Category"])
+    #grouped_df = grouped_df.groupby(["State", selected_group, "Product_Category"])
 
-    grouped_df = pd.DataFrame(grouped_df.sum().reset_index())
+    grouped_df = grouped_df.groupby(['State', selected_group, 'Product_Category']).agg({'Revenue': 'sum'})
+
+    #grouped_df = pd.DataFrame(grouped_df.sum().reset_index())
+
+    state_pcts = grouped_df.groupby(level=0).apply(lambda x: 100 * x / float(x.sum()))
+
+    grouped_df['Perc'] = state_pcts
+
+    grouped_df.reset_index(inplace=True)
 
     if selected_group == "Age_Group":
         name_sort = {'Youth (<25)': 0, 'Young Adults (25-34)': 1, 'Adults (35-64)': 2, 'Seniors (64+)': 3}
@@ -216,6 +256,9 @@ def update_output(selected_country, selected_state, selected_group, selected_yea
         grouped_df = grouped_df.drop('name_sort', axis=1)
     else:
         grouped_df = grouped_df
+
+    table_data = grouped_df.copy()
+    table_data['Revenue'] = table_data['Revenue'].map('${:,.0f}'.format)
 
     # Table
     table = html.Div([
@@ -227,7 +270,7 @@ def update_output(selected_country, selected_state, selected_group, selected_yea
                 {'name': 'Revenue', 'id': 'Revenue', 'type': 'numeric', 'editable': True}
             ],
             # columns=[{"name": i, "id": i} for i in grouped_df.columns],
-            data=grouped_df.to_dict('records'),
+            data=table_data.to_dict('records'),
             style_table={
                 'maxHeight': '20%',
                 # 'overflowY': 'scroll',
@@ -270,14 +313,14 @@ def update_output(selected_country, selected_state, selected_group, selected_yea
                 } if selected_group == "Age_Group" else
                 {
                     'if': {
-                        'filter_query': '{Customer_Gender} eq "F"',
+                        'filter_query': '{Customer_Gender} eq "Female"',
                     },
                     'backgroundColor': 'dodgerblue',
                     'color': 'white'
                 },
                 {
                     'if': {
-                        'filter_query': '{Customer_Gender} eq "M"',
+                        'filter_query': '{Customer_Gender} eq "Male"',
                     },
                     'backgroundColor': '#7FDBFF',
                     'color': 'white'
@@ -289,19 +332,22 @@ def update_output(selected_country, selected_state, selected_group, selected_yea
     # Plotly Express
     fig = px.bar(
         data_frame=grouped_df,
-        x=selected_group,
         y='Revenue',
+        x=selected_group,
         color='Product_Category',
+        text='Perc',
+        category_orders={"Product_Category": ["Clothing", "Bikes", "Accessories"]},
         opacity=0.6,
         color_discrete_map={
             'Accessories': '#0059b2',
             'Bikes': '#4ca6ff',
             'Clothing': '#99ccff'},
-        hover_data={'Revenue': ':,.0f'},
+        hover_data={'Revenue': ':$,.0f', 'Product_Category':True},
         labels={
             'Age_Group' if selected_group == 'Age_Group' else 'Customer_Gender': '<b>Age group</b>' if selected_group == 'Age_Group' else '<b>Gender</b>',
             'Revenue': '<b>Revenue</b>',
-            'Product_Category': '<b>Product category</b>'}
+            'Product_Category': '<b>Product Category</b>'
+        },
     )
 
     fig.update_layout(
@@ -313,8 +359,6 @@ def update_output(selected_country, selected_state, selected_group, selected_yea
         legend=dict(yanchor="bottom", y=1.02,
                     xanchor="right", x=1,
                     orientation="h"),
-        # bordercolor="Black",
-        # borderwidth=1.5),
         xaxis=dict(mirror=True, ticks='outside', showline=True, linewidth=1.5, linecolor='black'),
         yaxis=dict(mirror=True, ticks='outside', showline=True),
         margin=dict(l=50, r=20, t=30, b=20),
@@ -329,9 +373,8 @@ def update_output(selected_country, selected_state, selected_group, selected_yea
     fig.update_traces(
         marker_line_color='black',  # bar border color
         marker_line_width=1.5,
-        hovertemplate='<b>Age group</b>: %{x} <br>'
-                      # '<b>Product category</b>: %{name} <br>'
-                        '<b>Revenue</b>: %{y}')
+        texttemplate='%{text:.2f}%'
+    )
     # opacity=0.6)
 
     return container, fig, table
@@ -339,48 +382,59 @@ def update_output(selected_country, selected_state, selected_group, selected_yea
 
 @app.callback(
      Output(component_id='map', component_property='figure'),
-    [Input(component_id='slct_country', component_property='value')]
+    [Input(component_id='slct_country', component_property='value'),
+     Input(component_id='slct_year', component_property='value'),
+     Input(component_id='slct_group', component_property='value'),
+     Input(component_id='slct_subgrp', component_property='value')
+     ]
 )
-def update_my_map(selected_country):
-    data = df.loc[df["Country"] == selected_country]
+def update_my_map(selected_country, selected_year, selected_group, selected_subgroup):
 
-    data = data.groupby(['State', 'Product_Category'])
+    if selected_country == 'France':
+        container = "Select Department"
+    elif selected_country == 'Australia':
+        container = "Select Region"
+    elif selected_country == 'Canada':
+        container = "Select Province"
+    else:
+        container = "Select State"
+
+    #container = update_output()[0]
+    container = container.split(' ')[1]
+
+    data = df.loc[(df["Country"] == selected_country) & (df["Year"].isin(selected_year))]
+
+    data = data.groupby(['State', selected_group, 'Product_Category'])
     data = pd.DataFrame(data.sum().reset_index())
 
-    data.drop(data.columns.difference(['State', 'Product_Category', 'Revenue']), 1, inplace=True)
+    data.drop(data.columns.difference(['State', 'Product_Category', 'Revenue', selected_group]), 1, inplace=True)
 
-    data = data.pivot(index='State', columns='Product_Category', values='Revenue')
+    data = data.pivot(index=['State', selected_group], columns=['Product_Category'], values='Revenue')
 
     data = data.fillna(0)
 
-    data.reset_index(level=0, inplace=True)
+    data.reset_index(level=['State', selected_group], inplace=True)
+
+    data = data[data[selected_group] == selected_subgroup]
 
     if selected_country == 'United States':
         data['state_code'] = data.apply(lambda row: label_code(row), axis=1)
 
-    data['Revenue'] = data['Accessories'] + data['Bikes'] + data['Clothing']
+    data['Accessories2'] = data['Accessories'].groupby(data['State']).transform('sum')
 
-    if selected_country == 'Canada':
-        data.rename(columns={'State': 'name'}, inplace=True)
-    elif selected_country == 'Germany':
-        data.rename(columns={'State': 'NAME_1'}, inplace=True)
-    elif selected_country == 'France':
-        data.rename(columns={'State': 'nom'}, inplace=True)
-    elif selected_country == 'Australia':
-        data.rename(columns={'State': 'STATE_NAME'}, inplace=True)
-    else:
-        data.rename(columns={'State': 'State'}, inplace=True)
+    data['Bikes2'] = data['Bikes'].groupby(data['State']).transform('sum')
 
-    for col in data.columns:
-        data[col] = data[col].astype(str)
+    data['Clothing2'] = data['Clothing'].groupby(data['State']).transform('sum')
 
-    data['text'] = 'State: ' + data.iloc[:, 0] + '<br>' + \
-               'Accessories Rev: $' + data['Accessories'] + '<br>' + \
-               'Bikes Rev: $' + data['Bikes'] + '<br>' + \
-               'Clothing Rev: $' + data['Clothing'] + '<br>' + \
-               'Total Rev: $' + data['Revenue']
+    data['Revenue'] = data['Accessories2'] + data['Bikes2'] + data['Clothing2']
 
-    #data['Revenue'] = data['Revenue'].apply(pd.to_numeric)
+    data.rename(columns={'State': 'id'}, inplace=True)
+
+    data = data.sort_values(by=['Revenue'], ascending=False)
+
+    data['Revenue'] = data['Revenue'].map('${:,.0f}'.format)
+
+    data['Revenue'] = data['Revenue'].astype(str)
 
     if selected_country == "United States":
         my_map = px.choropleth(
@@ -389,72 +443,123 @@ def update_my_map(selected_country):
             locations='state_code',
             scope="usa",
             color='Revenue',
-            hover_data=['text']
+            hover_name="id",
+            hover_data={'id': False,
+                        'state_code': False,
+                        'Accessories2': ':$,.0f',
+                        'Bikes2': ':$,.0f',
+                        'Clothing2': ':$,.0f'},
+            labels={'Revenue': 'Total Revenue',
+                    'Accessories2': 'Accessories',
+                    'Bikes2': 'Bikes',
+                    'Clothing2': 'Clothing'
+                    },
+            title='<b>USA map</b>'
             #template='plotly_dark'
         )
-        # my_map = go.Figure(
-        #     data=[
-        #         go.Choropleth(
-        #             #colorbar=dict(title='Revenue', ticklen=3),
-        #             locationmode='USA-states',
-        #             locations=data['state_code'],
-        #             z=data["Revenue"].astype(float),
-        #             colorscale='Reds',
-        #             text=data['text'],
-        #         ),
-        #     ],
-        #     layout=dict(geo={'subunitcolor': 'black'})
-        # )
-        #
-        # my_map.update_layout(
-        #     title_text='2011 US Agriculture Exports by State',
-        #     geo_scope='usa',
-        #     dragmode=False)
+
+        my_map.add_scattergeo(
+            locationmode='USA-states',
+            locations=data['state_code'],
+            text=data['state_code'],
+            mode='text',
+            hoverinfo='none',
+            showlegend=False
+        )
 
     else:
         if selected_country == 'Canada':
             country_url='https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/canada.geojson'
-            json_state = 'name'
             scope = 'north america'
         elif selected_country == 'France':
             country_url='https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/departements-version-simplifiee.geojson'
-            json_state = 'nom'
             scope = 'europe'
+
         elif selected_country == 'Germany':
             country_url='https://gist.githubusercontent.com/oscar6echo/4423770/raw/990e602cd47eeca87d31a3e25d2d633ed21e2005/dataBundesLander.json'
-            json_state = 'NAME_1'
             scope = 'europe'
         else:
             country_url='https://raw.githubusercontent.com/tonywr71/GeoJson-Data/master/australian-states.json'
-            json_state = 'STATE_NAME'
             scope = 'world'
 
         json_data = requests.get(country_url).json()
-        tuple_feature_id = ('properties', json_state)
+
+        for i in json_data["features"]:
+            property = i['properties']
+            if selected_country == "Australia":
+                property['id'] = property.pop('STATE_NAME')
+            elif selected_country == "Canada":
+                property['id'] = property.pop('name')
+            elif selected_country == "France":
+                property['id'] = property.pop('nom')
+            else:
+                property['id'] = property.pop('NAME_1')
+
+        map_title = (selected_country, 'map')
 
         my_map = px.choropleth(
             data_frame=data,
             geojson=json_data,
-            locations=json_state,
-            featureidkey=".".join(tuple_feature_id),
-            color='Revenue',
-            hover_data=['text'],
+            locations='id',
+            featureidkey='properties.id',
+            color='id' if selected_country == 'France' else 'Revenue',
+            hover_data={
+                        'Accessories2': ':$,.0f',
+                        'Bikes2': ':$,.0f',
+                        'Clothing2': ':$,.0f',
+                        'Revenue': True},
+            labels={
+                'Revenue': 'Total Revenue',
+                'id': container,
+                'Accessories2': 'Accessories',
+                'Bikes2': 'Bikes',
+                'Clothing2': 'Clothing'
+            },
             color_continuous_scale='Magma',
-            scope=scope
+            scope=scope,
+            title='<b>'+" ".join(map_title) + '</b>',
+            center= dict(lat=48.864716, lon=2.349014) if selected_country == 'France' else None,
         )
 
         my_map.update_geos(
-            visible=False,
+            visible=False,          # hide the base map and frame
             showcountries=True,
             showcoastlines=False,
             showland=False,
-            fitbounds="locations",
-            showsubunits=True,
-            subunitcolor="Blue",
+            fitbounds="locations",   # automatically zoom the map to show just the area of interest
             resolution=110
         )
 
-        my_map.update_layout(dragmode=False)
+        my_map.add_scattergeo(
+            geojson=json_data,
+            locations=data['id'],
+            text=None if selected_country == 'France' else data['id'],
+            showlegend=False,
+            featureidkey="properties.id",
+            mode='text',
+            hoverinfo='none'
+        )
+
+    my_map.update_traces(
+        marker_line_width=1,
+        marker_line_color='white'
+    )
+
+    my_map.update_layout(
+        #dragmode=False,
+        margin=dict(l=20, r=0, t=50, b=20),
+        legend_title_text='<b>Total Revenue</b>',
+        legend=dict(
+            yanchor="top",
+            y=0.99,
+            xanchor="left",
+            x=0.01,
+            font=dict(size=12)
+        ),
+        title_x=0.5,
+        uniformtext_minsize=12,
+        uniformtext_mode='hide'
+    )
 
     return my_map
 
--- map.py
+++ map.py
@@ -1,47 +1,22 @@
 import pandas as pd
 import requests
-import plotly
-import plotly
-print(plotly.__version__)
 
+df = pd.read_csv(
+    'https://raw.githubusercontent.com/ine-rmotr-curriculum/FreeCodeCamp-Pandas-Real-Life-Example/master/data/sales_data.csv')
 
+df.drop(
+    df.columns.difference(['Customer_Gender',  'Product_Category', 'Revenue', 'State', 'Country', 'Year']),
+    1, inplace=True)
 
-df = pd.read_csv('https://raw.githubusercontent.com/ine-rmotr-curriculum/FreeCodeCamp-Pandas-Real-Life-Example/master/data/sales_data.csv')
+data = df.loc[(df["Country"] == "France")]
 
+data = data.groupby(['State', 'Customer_Gender', 'Product_Category']).agg({'Revenue': 'sum'})
+#data = pd.DataFrame(data.sum().reset_index())
 
-uk = ['United Kingdom', 'United States']
-mask = df['Country'].str.contains(uk, na=False, regex=True, case=False)
-df = df[~mask]
-print(df.Country.unique())
+state_pcts = data.groupby(level=0).apply(lambda x: 100 * x / float(x.sum()))
 
-canada_url = 'https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/canada.geojson'
-france_url = 'https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/departements-version-simplifiee.geojson'
-germany_url = 'https://gist.githubusercontent.com/oscar6echo/4423770/raw/990e602cd47eeca87d31a3e25d2d633ed21e2005/dataBundesLander.json'
-australia_url = 'https://raw.githubusercontent.com/tonywr71/GeoJson-Data/master/australian-states.json'
+data['Perc'] = state_pcts
 
+data.reset_index(inplace=True)
 
-data = requests.get(canada_url).json()
-
-
-def my_function(country, country_url):
-    data = requests.get(country_url).json()
-
-    for i in data["features"]:
-        property = i['properties']
-        if country == "Australia":
-            property['id'] = property.pop('STATE_NAME')
-        elif country == "Canada":
-            property['id'] = property.pop('name')
-        elif country == "France":
-            property['id'] = property.pop('nom')
-        else:
-            property['id'] = property.pop('NAME_1')
-
-    return data["features"][0]
-
-
-my_function(country='Australia',
-            country_url='https://raw.githubusercontent.com/tonywr71/GeoJson-Data/master/australian-states.json')
-
-# print(data["features"][0])
-
+print(data)
