6556258cf37423153547658cdd59ddd0f6fabedd
MinhChauVanNguyen <minh.chau@outlook.co.nz>
2020-12-05 19:08:24 +1300

    m

--- app.py
+++ app.py
@@ -7,6 +7,6 @@ app = dash.Dash(
     external_stylesheets=[dbc.themes.BOOTSTRAP],
     suppress_callback_exceptions=True  # remove "Id not found in layout" message
 )
-# import index
+import index
 
 server = app.server
