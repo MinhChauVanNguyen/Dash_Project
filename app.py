cb348adc8a8f454e88f35dcc16e47991e6165fa0
Minh Chau <61895317+MinhChauVanNguyen@users.noreply.github.com>
2023-04-16 19:34:03 +1200

    update app

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ Created on Tues, 27 Oct 2020
 
 import dash
 import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
+import dash_html_components as html
+from Tabs import Sidebar, Tab1, Tab2, Tab3, Navbar
 
 app = dash.Dash(
     __name__,
@@ -14,3 +17,24 @@ app = dash.Dash(
 )
 
 server = app.server
+
+app.layout = html.Div(
+    children=[
+        Navbar.layout,
+        Sidebar.layout
+    ])
+
+
+@app.callback(Output(component_id='tabs-content', component_property='children'),
+              [Input(component_id='tabs', component_property='value')])
+def render_content(tab):
+    if tab == 'tab-1':
+        return Tab1.layout
+    elif tab == 'tab-2':
+        return Tab2.layout
+    elif tab == 'tab-3':
+        return Tab3.layout
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
